b0a3c8a4469eb6cc16509e11cc9a200c56ae2571
xyz <qq@qq.com>
2024-11-01 11:01:25 +0800

    solve structs2

--- exercises/structs/structs2.rs
+++ exercises/structs/structs2.rs
@@ -30,6 +30,18 @@ fn create_order_template() -> Order {
     }
 }
 
+fn creat_my_order() -> Order {
+    Order {
+        name: String::from("Hacker in Rust"),
+        year: 2019,
+        made_by_phone: false,
+        made_by_mobile: false,
+        made_by_email: true,
+        item_number: 123,
+        count: 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,14 +49,15 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
+        let my_order = creat_my_order();
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
-        assert_eq!(your_order.name, "Hacker in Rust");
-        assert_eq!(your_order.year, order_template.year);
-        assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
-        assert_eq!(your_order.made_by_mobile, order_template.made_by_mobile);
-        assert_eq!(your_order.made_by_email, order_template.made_by_email);
-        assert_eq!(your_order.item_number, order_template.item_number);
-        assert_eq!(your_order.count, 1);
+        assert_eq!(my_order.name, "Hacker in Rust");
+        assert_eq!(my_order.year, order_template.year);
+        assert_eq!(my_order.made_by_phone, order_template.made_by_phone);
+        assert_eq!(my_order.made_by_mobile, order_template.made_by_mobile);
+        assert_eq!(my_order.made_by_email, order_template.made_by_email);
+        assert_eq!(my_order.item_number, order_template.item_number);
+        assert_eq!(my_order.count, 1);
     }
 }
